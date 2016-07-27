b3f591c90873aada09e522ffc4320fdc75183dbd
Anthony Sottile <asottile@umich.edu>
2016-07-26 23:56:40 -0700

    Move js to a separate file (and delete most of it)

---
+++ static/js/main.js
@@ -0,0 +1,99 @@
+(function () {
+    var map, markerCache = {};
+
+    function zeroPad(n) {
+        return ('0' + n).slice(-2);
+    }
+
+    function setLabelTime() {
+        $('.label-countdown').each(function (index, element) {
+            var difference, minutes, seconds, timestring,
+                disappearsAt = new Date(parseInt(element.getAttribute('disappears-at')));
+
+            difference = disappearsAt - new Date();
+            if (difference < 0) {
+                timestring = '(expired)';
+            } else {
+                minutes = Math.floor(difference / (60 * 1000));
+                difference %= (60 * 1000);
+                seconds = Math.floor(difference / 1000);
+
+                timestring = [
+                    '(', zeroPad(minutes), 'm', zeroPad(seconds), 's', ')'
+                ].join('');
+            }
+
+            $(element).text(timestring)
+        });
+    };
+
+    function createMap() {
+        var center = new google.maps.LatLng(
+            window.POGO.originLat, window.POGO.originLng
+        );
+        map = new google.maps.Map(
+            document.getElementById('fullmap'), {
+                center: center,
+                zoom: window.POGO.zoom,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoomControl: true,
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                rotateControl: true,
+                fullscreenControl: true
+        });
+        new google.maps.Marker({
+            position: center,
+            map: map,
+            icon: '//maps.google.com/mapfiles/ms/icons/red-dot.png'
+        });
+    }
+
+    function addMarker(item) {
+        if (item.key in markerCache) {
+            return;
+        }
+
+        var disappearsMs = new Date(item.disappear_time) - new Date();
+        if (disappearsMs < 0) {
+            return;
+        }
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item.lat, item.lng),
+            map: map,
+            icon: item.icon,
+        });
+        markerCache[item.key] = marker;
+
+        var infoWindow = new google.maps.InfoWindow({content: item.infobox});
+        marker.addListener('click', infoWindow.open.bind(infoWindow, map, marker));
+
+        window.setTimeout(
+            function () {
+                markerCache[item.key].setMap(null);
+                delete markerCache[item.key];
+            },
+            disappearsMs
+        );
+    }
+
+    function updateMap() {
+        $.get('/data', function(data) {
+            for (var i = 0; i < data.length; i += 1) {
+                addMarker(data[i]);
+            }
+        });
+    }
+
+    window.initMap = function () {
+        createMap();
+        updateMap();
+        $('.refresh').on('click', updateMap);
+        if (window.POGO.autoRefresh) {
+            window.setInterval(updateMap, window.POGO.autoRefresh);
+        }
+    }
+
+    window.setInterval(setLabelTime, 1000);
+})();
